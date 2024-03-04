3f5af22e0b78d30550551b02e66d2f2efdf5acc5
jenniferchristine <jeja2306@student.miun.se>
2024-03-04 15:16:03 +0100

    basic api testing

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,41 @@ function toggleMenu() {
         navMenuEl.style.display = "none";
     }
 }
+
+window.onload = () => {
+    search();
+}
+
+async function search() {
+    const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=love+the+way+you+lie";
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '909254ca7bmsh60eaea82866cd6dp1c9572jsnc5023f43402d',
+            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+        }
+    };
+
+    const response = await fetch(url, options);
+
+    if (response.ok) {
+        const data = await response.json();
+        /* getLyrics(data.data[0].id); */
+        getLyrics();
+    } else {
+        console.log("ERROR: " + response.statusText);
+    }
+}
+
+async function getLyrics(id) {
+    const url = "https://api.lyrics.ovh/v1/coldplay/adventure+of+a+lifetime";
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data.lyrics);
+    } else {
+        console.log("ERROR");
+    }
+}
