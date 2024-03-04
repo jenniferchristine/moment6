70392038a6d4e7efc5e9473bf3117999ce123c5e
jenniferchristine <jeja2306@student.miun.se>
2024-03-04 22:31:42 +0100

    added color to icons

--- src/js/main.js
+++ src/js/main.js
@@ -78,11 +78,10 @@ function printSongs(result) {
         album.appendChild(albumText);
         textContainer.appendChild(album);
 
-        var svgCode = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"/></svg>';
-        var dataURL = 'data:image/svg+xml;base64,' + btoa(svgCode);
-        const playButton = document.createElement("img");
-        playButton.src = dataURL;
-        playButton.classList.add("playbutton");
+
+        const playButton = document.createElement("span");
+        playButton.classList.add("material-symbols-outlined");
+        playButton.innerHTML = "play_circle";
         textContainer.appendChild(playButton);
 
         container.appendChild(textContainer);
