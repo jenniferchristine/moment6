66c7679cf6812e2348865d7ab28065e050bbb153
jenniferchristine <jeja2306@student.miun.se>
2024-03-04 16:20:21 +0100

    changed styling for nav for an app-looking-layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,5 @@
 "use strict"
 
-let openBtn = document.getElementById("open-menu");
-let closeBtn = document.getElementById("close-menu");
-
-openBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    let navMenuEl = document.getElementById("nav-menu");
-    let style = window.getComputedStyle(navMenuEl);
-
-    if (style.display === "none") {
-        navMenuEl.style.display = "block";
-    } else {
-        navMenuEl.style.display = "none";
-    }
-}
-
 window.onload = () => {
     const searchValue = document.getElementById("searchValue");
 
